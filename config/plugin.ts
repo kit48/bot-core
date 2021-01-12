6cf3a0c7e6be868125ac392a6b11e47ae585f9de
zpr1g <yuns.xie@qq.com>
2021-01-12 22:28:45 +0800

    feat: resolve paths

--- config/plugin.ts
+++ config/plugin.ts
@@ -1,3 +1,5 @@
+/** ref: https://eggjs.org/zh-cn/tutorials/typescript.html#%E5%9C%A8-tsconfigjson-%E4%B8%AD%E9%85%8D%E7%BD%AE%E4%BA%86-paths-%E6%97%A0%E6%95%88 */
+import 'tsconfig-paths/register';
 import { EggPlugin } from 'egg';
 
 const plugin: EggPlugin = {
